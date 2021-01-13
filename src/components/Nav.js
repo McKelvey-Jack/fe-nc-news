ac97fc1219f4f1813bd70a32405176638084a72c
Jack McKelvey <71670664+McKelvey-Jack@users.noreply.github.com>
2021-01-13 13:33:31 +0000

    moved the nev bat to the center

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from '@reach/router';
 import * as api from '../api';
-import DropdownMenu from './DropdownMenu';
+// import DropdownMenu from './DropdownMenu';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHome,
+  faUser,
+  faAngleDoubleUp,
+  faBars,
+} from '@fortawesome/free-solid-svg-icons';
 
 export default class Nav extends Component {
-  state = { topics: [] };
+  state = { topics: [], showMenu: false };
 
   componentDidMount() {
     api.getTopics().then((topics) => {
       this.setState({ topics });
     });
+  }
+
+  showMenu() {
+    this.setState((currState) => {
+      let newState = null;
+      if (currState.showMenu === false) {
+        newState = true;
+      } else {
+        newState = false;
+      }
+      return { showMenu: newState };
+    });
+  }
+
+  hideMenu() {
+    this.setState({ showMenu: false });
   }
 
   render() {
@@ -28,8 +49,40 @@ export default class Nav extends Component {
               <FontAwesomeIcon icon={faUser} />
             </button>
           </Link>
-          <DropdownMenu />
+          <button
+            className={'menu-button'}
+            onClick={() => {
+              this.showMenu();
+            }}
+          >
+            {!this.state.showMenu ? (
+              <FontAwesomeIcon icon={faBars} />
+            ) : (
+              <FontAwesomeIcon icon={faAngleDoubleUp} />
+            )}
+          </button>
         </nav>
+        <div className={'menu'}>
+          {this.state.showMenu ? (
+            <div className={'menu-container'}>
+              {this.state.topics.map((topic) => {
+                return (
+                  <div className={'drop-down-content'}>
+                    <Link
+                      onClick={() => {
+                        this.hideMenu();
+                      }}
+                      key={topic.slug}
+                      to={`articles/${topic.slug}`}
+                    >
+                      <button className={'topic-button'}>{topic.slug}</button>
+                    </Link>
+                  </div>
+                );
+              })}
+            </div>
+          ) : null}
+        </div>
       </header>
     );
   }
